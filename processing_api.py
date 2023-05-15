67128c72efb139a867857e913217ece5218098a4
MHindermann <maximilian.hindermann@gmail.com>
2023-05-15 11:15:39 +0200

    API test

---
+++ processing_api.py
@@ -0,0 +1,136 @@
+""" processing_api.py
+=============
+TranskribusProcessingAPI class.
+"""
+
+import requests
+
+
+class TranskribusProcessingAPI:
+    """ Wrapper class of the Transkribus Processing API.
+
+    Swagger documentation of the API at https://transkribus.eu/processing/swagger/.
+    """
+
+    def __init__(self) -> None:
+        self.base_url = "https://transkribus.eu/processing/v1"
+        self.refresh_token = None
+        self.setup()
+
+    def setup(self):
+        try:
+            from credentials import USER, PASSWORD
+            response = self.authenticate(user=USER,
+                                         password=PASSWORD)
+            if response.status_code != 200:
+                print(f"{response.json()}")
+                raise ConnectionError
+            self.refresh_token = response.json()["access_token"]
+            print("Authorization successful.")
+        except ImportError:
+            raise
+
+    @staticmethod
+    def authenticate(user: str,
+                     password: str) -> requests.Response:
+        """ Wrapper of oAuth2AuthCode.
+
+        :param user: the username
+        :param password: the password
+        """
+
+        data = {
+            "grant_type": "password",
+            "username": user,
+            "password": password,
+            "client_id": "processing-api-client",
+        }
+
+        response = requests.post("https://account.readcoop.eu/auth/realms/readcoop/protocol/openid-connect/token",
+                                 data=data)
+
+        return response
+
+    def post_processes(self,
+                       line_model_id: int,
+                       htr_model_id: int,
+                       image: str
+                       ) -> requests.Response:
+        """ Wrapper of https://transkribus.eu/processing/swagger/#/Submit%20data%20for%20processing. """
+
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {self.refresh_token}",
+            "Content-Type": "application/json",
+        }
+
+        data = {
+            "config": {
+                "lineDetection": {
+                    "modelId": line_model_id,
+                },
+                "textRecognition": {
+                    "htrId": htr_model_id,
+                },
+            },
+            "image": {
+                "base64": image,
+            },
+
+        }
+
+        response = requests.post(f"{self.base_url}/processes",
+                                 headers=headers,
+                                 json=data)
+
+        return response
+
+    def get_result(self,
+                   process_id: int):
+        """ Wrapper of https://transkribus.eu/processing/swagger/#/Retrieve%20processing%20status%20and%20result. """
+
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {self.refresh_token}",
+        }
+
+        response = requests.get(f"{self.base_url}/processes/{process_id}",
+                                headers=headers)
+
+        return response
+
+    def get_user(self) -> requests.Response:
+        """ Wrapper of https://transkribus.eu/processing/swagger/#/User%20Account/getUserInfo. """
+
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {self.refresh_token}",
+        }
+
+        response = requests.get(f"{self.base_url}/user",
+                                headers=headers)
+
+        return response
+
+
+def tester():
+    """ works fine """
+    api = TranskribusProcessingAPI()
+
+    get_response = api.get_result(process_id=4982858)
+    print(get_response.json())
+
+    exit()
+
+    from sample_image import SAMPLE_IMAGE
+    post_response = api.post_processes(line_model_id=49272,  # Mixed Text Line Orientation
+                                       htr_model_id=39995,  # Transkribus Print M1
+                                       image=SAMPLE_IMAGE)
+    print(post_response.json())
+    process_id = post_response.json()["processId"]
+    print(process_id)
+    get_response = api.get_result(process_id=process_id)
+    print(get_response.json())
+
+
+tester()
