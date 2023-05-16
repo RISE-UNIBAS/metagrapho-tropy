5e5fe216840823b23cfd2bbd3f77ed3e09743707
MHindermann <50413752+MHindermann@users.noreply.github.com>
2023-05-16 14:58:52 +0200

    con't integration: pathing

--- metagrapho_tropy/main.py
+++ metagrapho_tropy/main.py
@@ -11,4 +11,5 @@ PARENT_DIR = os.path.dirname(os.path.dirname(__file__))
 TEST = f"{PARENT_DIR}/test"
 
 Client().enrich(tropy_file_path=f"{TEST}/sample_input.json",
-                item_image_index=1)
+                item_image_index=1,
+                lowest_common_dir="C:/Users/hinder0000/PycharmProjects")
